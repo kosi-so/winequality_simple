d7cb4db4865c555ae45ccda95bfe3e5309d27163
Kosi Akaeze <kossyfab@gmail.com>
2023-01-28 19:26:28 +1100

    tracker added

--- src/get_data.py
+++ src/get_data.py
@@ -16,7 +16,7 @@ def get_data(config_path):
     config = read_params(config_path)
     data_path = config['data_source']['s3_source']
     df = pd.read_csv(data_path, sep=",", )
-    return df.head()
+    return df
 
 if __name__ == "__main__":
     args = argparse.ArgumentParser()
