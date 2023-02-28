0fc14c0cf8582a95e90dca1cef29becca1a7eb11
Kosi Akaeze <kossyfab@gmail.com>
2023-03-01 09:26:35 +1100

    update app.py

--- app.py
+++ app.py
@@ -29,7 +29,8 @@ def index():
         except Exception as e:
             print(e)
             # error = {"error": "Something's not right, Try Again! "}
-            return render_template("404.html", error=e)
+            error = {"error": e}
+            return render_template("404.html", error= error)
         
     else:
         return render_template('index.html')
